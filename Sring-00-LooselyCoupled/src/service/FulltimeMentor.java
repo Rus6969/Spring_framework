69940214aab8ba7135f61af67a9ff23314a2faff
Russell_Samatov <russam4515@gmail.com>
2020-10-19 20:56:30 -0400

    loseley coupled

---
+++ Sring-00-LooselyCoupled/src/service/FulltimeMentor.java
@@ -0,0 +1,11 @@
+package service;
+
+import implementation.Mentor;
+
+public class FulltimeMentor implements Mentor {
+    @Override
+    public void createAccount() {
+        System.out.println("Fulltime Mentor Account is created");
+
+    }
+}
