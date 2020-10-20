8f7985ef8094de1a7f793274bf51ee76ea465361
Russell_Samatov <russam4515@gmail.com>
2020-10-19 21:23:05 -0400

    loseley coupled

--- Sring-00-LooselyCoupled/src/CyberAp.java
+++ Sring-00-LooselyCoupled/src/CyberAp.java
@@ -1,12 +1,16 @@
 import implementation.Mentor;
 import service.FulltimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CyberAp {
     public static void main(String[] args) {
-        FulltimeMentor fulltimeMentor= new FulltimeMentor();
+        //FulltimeMentor fulltimeMentor= new FulltimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
 
-        MentorAccount mentor = new MentorAccount(fulltimeMentor) ;
+        MentorAccount mentor = new MentorAccount(partTimeMentor) ;
         mentor.manageAccount();
+
+
     }
 }
