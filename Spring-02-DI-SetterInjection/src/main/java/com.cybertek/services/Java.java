f50e00f2e95a1daeffa22ad7cf91bf114a8af70a
Russell_Samatov <russam4515@gmail.com>
2020-10-21 21:11:21 -0400

    Spring SetterInjection

--- Spring-02-DI-SetterInjection/src/main/java/com.cybertek/services/Java.java
+++ Spring-02-DI-SetterInjection/src/main/java/com.cybertek/services/Java.java
@@ -1,17 +1,25 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import lombok.Data;
 
+@Data
 public class Java implements Course {
-OfficeHours officeHours;
+    private ExtraSessions extraSessions;
+  //  private OfficeHours officeHours;
+// here @Data gets for us getters and setters or we can do command N create getters setters
+//    public OfficeHours getOfficeHours() {
+//        return officeHours;
+//    }
+//
+//    public void setOfficeHours(OfficeHours officeHours) {// set +officeHours
+//        this.officeHours = officeHours;
+//    }
 
-    public Java(OfficeHours officeHours) {
-        this.officeHours = officeHours;
-    }
-
-    @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours "+(20+officeHours.getHours()));
+        System.out.println("Weekly Teaching Hours : " + (20 + extraSessions.getHours()));
+
 
     }
 }
