d06886bf11b866b21a6dd755c86022b3a7c9b7ef
Russell_Samatov <russam4515@gmail.com>
2020-10-28 17:50:34 -0400

    Spring Value Annotation

--- Spring-05-Annotations-Autowired/src/main/java/com/cybertek/services/Java.java
+++ Spring-05-Annotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -1,19 +1,28 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
+     //@Autowired
+    private ExtraSessions officeHours;
 
-    private  OfficeHours officeHours;
-
-     @Autowired
+   //  @Autowired
     public Java(OfficeHours officeHours) {
         this.officeHours = officeHours;
     }
+    /*
+    //Setter Injection
+    @Autowired
+    public void setOfficeHours(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+ */
 
     @Override
     public void getTeachingHours() {
