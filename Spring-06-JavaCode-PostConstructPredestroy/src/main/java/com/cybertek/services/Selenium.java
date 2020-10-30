d7acfee3975b3d11ef3842189a4e2573fdb68c85
Russell_Samatov <russam4515@gmail.com>
2020-10-30 11:31:49 -0400

    Spring PostConstruct - Predestroy

---
+++ Spring-06-JavaCode-PostConstructPredestroy/src/main/java/com/cybertek/services/Selenium.java
@@ -0,0 +1,26 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class Selenium implements Course {
+    @Override
+    public void getTeachingHourse() {
+        System.out.println("Java teaching Hours 30");
+
+    }
+
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("Executing PostConstruct");
+    }
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("Executing PostDestroy");
+    }
+}
