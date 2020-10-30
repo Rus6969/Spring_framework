7d7835bb4e4d63826e9c9671276050f130ec3f76
Russell_Samatov <russam4515@gmail.com>
2020-10-29 20:08:22 -0400

    Spring config no xml

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
@@ -1,5 +1,9 @@
 package com.cybertek.configs;
 
+import com.cybertek.services.Java;
+import com.cybertek.services.OfficeHours;
+import com.cybertek.services.Selenium;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +11,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.cybertek")
 public class CybertekAppConfig {
+
+    @Bean
+    public Java java (){
+        return new Java(officeHours());
+    }
+
+    @Bean
+    public Selenium selenium(){
+        return  new Selenium();
+    }
+    @Bean
+    public OfficeHours officeHours(){
+        return new OfficeHours();
+    }
 
 }
--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,12 +1,21 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Java implements Course {
+
+
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     @Override
     public void teachingHours() {
-        System.out.println("Weekly teachig classes for Java 30");
+        System.out.println("Weekly teachig classes for Java " +(30+extraSessions.getHours()));
     }
 }
