11c2a342084871f9e721f7212c3024420c381dad
Russell_Samatov <russam4515@gmail.com>
2020-11-14 13:00:28 -0500

    Thyleafe

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/StudentController.java
@@ -1,12 +1,14 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-    @RequestMapping("/show")
+
+    @GetMapping("/show")
     public String showForm(){
         return "home";
     }
