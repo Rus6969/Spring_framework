f09736d9a07cbbb85da981c326bd658dff19ee39
Russell_Samatov <russam4515@gmail.com>
2021-01-17 23:19:14 -0500

    data for new tables populated

---
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -0,0 +1,14 @@
+package com.cybertek.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private Long studentID;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
