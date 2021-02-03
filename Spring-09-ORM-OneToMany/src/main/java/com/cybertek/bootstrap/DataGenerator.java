89c5abbcefe7ef82ce92e9219c3beea0f68cc3a2
Russell_Samatov <russam4515@gmail.com>
2021-02-03 14:06:55 -0500

    One to Many

---
+++ Spring-09-ORM-OneToMany/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -0,0 +1,26 @@
+package com.cybertek.bootstrap;
+
+import com.cybertek.entity.Address;
+import com.cybertek.entity.Person;
+import com.cybertek.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+    @Autowired
+    PersonRepository personRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        Person p1 = new Person("Ruslan", "Samatov");
+        Person p2 = new Person("Inna","Gorbunova");
+        Person p3 = new Person("Miho","Erohin");
+
+
+        Address a1 = new Address("King St","22042");
+        Address a2 = new Address("Elm St","22102");
+        Address a3 = new Address("Java St","22036");
+
+    }
+}
---
+++ Spring-09-ORM-OneToMany/src/main/java/com/cybertek/entity/Person.java
@@ -0,0 +1,35 @@
+package com.cybertek.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String firstName;
+    private String lastName;
+    // in one to many we need create container one person needs to match many addresses  we need put it as a collection
+    @OneToMany(mappedBy = "person")
+    private List<Address> addresses;
+
+
+    //case-1
+//    @OneToMany(cascade = CascadeType.ALL)
+//    @JoinColumn(name="person_id")
+//    private List<Address> addresses;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
