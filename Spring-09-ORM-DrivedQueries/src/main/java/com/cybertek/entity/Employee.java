51100cbaed20a510ecbe5996b711853fd163ebad
Russell_Samatov <russam4515@gmail.com>
2021-02-18 15:03:03 -0500

    data is pushed

--- Spring-09-ORM-DrivedQueries/src/main/java/com/cybertek/entity/Employee.java
+++ Spring-09-ORM-DrivedQueries/src/main/java/com/cybertek/entity/Employee.java
@@ -7,8 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Set;
+
 
 @Entity
 @Table(name = "employees")
@@ -21,7 +20,7 @@ public class Employee extends BaseEntity {
     @Column(name = "last_name")
     private String lastName;
     @Column(name = "email")
-    private  String email;
+    private String email;
 
     @Column(columnDefinition = "DATE")
     private LocalDate hireDate;
@@ -32,12 +31,14 @@ public class Employee extends BaseEntity {
     private Long salary;
 
 
-
     @ManyToOne
     // we need joincolumn here to specify to specify foreign name of deaprtment by defauilt it would create department_id but
     // since we are using data.sql to pump data there is name department( thats why we are using this annotation)
     @JoinColumn(name = "department")
     private Department departments;
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
 
 
 }
