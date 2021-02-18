818574c10d089223af8c80dc6027cb9242a96fb1
Russell_Samatov <russam4515@gmail.com>
2021-02-18 00:37:31 -0500

    bootstrap option pass data throught commandliner

---
+++ Spring-09-ORM-CinemaLab/src/main/java/cinemaLab/repository/MovieCinemaRepository.java
@@ -0,0 +1,9 @@
+package cinemaLab.repository;
+
+import cinemaLab.entity.MovieCinema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long > {
+}
---
+++ Spring-09-ORM-CinemaLab/src/main/java/cinemaLab/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package cinemaLab.repository;
+
+import cinemaLab.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
