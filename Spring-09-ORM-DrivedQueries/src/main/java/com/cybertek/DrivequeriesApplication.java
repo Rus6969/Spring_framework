67260778f91c3a693d557a736b0c08cac8ca530c
Russell_Samatov <russam4515@gmail.com>
2021-02-18 13:28:26 -0500

    queries in Spring boot / drived and custom

---
+++ Spring-09-ORM-DrivedQueries/src/main/java/com/cybertek/DrivequeriesApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivequeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivequeriesApplication.class, args);
+	}
+
+}
---
+++ Spring-09-ORM-DrivedQueries/src/main/java/com/cybertek/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.cybertek.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository {
+}
