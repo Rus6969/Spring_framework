85c2dccef4c685f19418aa183ef494d001861f59
Russell_Samatov <russam4515@gmail.com>
2021-02-18 16:28:05 -0500

    Region repository query

--- Spring-09-ORM-DrivedQueries/src/main/java/com/cybertek/DrivequeriesApplication.java
+++ Spring-09-ORM-DrivedQueries/src/main/java/com/cybertek/DrivequeriesApplication.java
@@ -1,13 +1,37 @@
 package com.cybertek;
 
+import com.cybertek.repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DrivequeriesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DrivequeriesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DrivequeriesApplication.class, args);
+    }
+
+    @Autowired
+    RegionRepository regionRepository;
+
+    @PostConstruct
+    public void testRegion() {
+        System.out.println("---------Regions start------------");
+
+        System.out.println("findByCountry: " + regionRepository.findByCountry("Canada"));
+        System.out.println("findDistinctByCountry: " + regionRepository.findDistinctByCountry("Canada"));
+        System.out.println("findByCountryContaining: " + regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainingOrderByCountry: " + regionRepository.findByCountryContainingOrderByCountry("Asia"));
+        System.out.println("findByCountryContainingOrderByCountry: " + regionRepository.findByCountryContainingOrderByCountry("Asia"));
+        System.out.println("findTopBy2ByCountry: " + regionRepository.findTop2ByCountry("Canada"));
+
+        System.out.println("---------Regions End------------");
+
+
+
+    }
 
 }
