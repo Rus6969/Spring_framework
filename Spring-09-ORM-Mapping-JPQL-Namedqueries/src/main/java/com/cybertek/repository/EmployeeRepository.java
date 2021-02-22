b92862bcfd0a1a1032278e45206d1d53dcb1b13e
Russell_Samatov <russam4515@gmail.com>
2021-02-22 00:00:39 -0500

    blind parameters

--- Spring-09-ORM-Mapping-JPQL-Namedqueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-Namedqueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,7 +35,43 @@ public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     @Query("select e from Employee e where e.firstName=:name OR e.salary=:salary")
     List<Employee>getEmployeeByFirstNameOrSalary(@Param("name")String name,@Param("salary") int salary);
 
+    //NOT equal
+    @Query("select e from Employee e where e.salary <>?1")
+    List<Employee>getEmployeeBySalaryNotEqual(int salary);
 
+
+
+    //Like / Contains / StartsWith / Ends With
+    @Query("SELECT e FROM Employee e WHERE e.firstName LIKE ?1")
+    List<Employee> getEmployeeByFirstNameLike(String patter);
+
+    //Less Than
+    @Query("SELECT e FROM Employee e WHERE e.salary < ?1")
+    List<Employee> getEmployeeBySalaryLessThan(int salary);
+
+    //Greater Than
+    @Query("SELECT e FROM Employee e WHERE e.salary > ?1")
+    List<Employee> getEmployeeBySalaryGreaterThan(Integer salary);
+
+    //Between
+    @Query("SELECT e FROM Employee e WHERE e.salary BETWEEN ?1 AND ?2")
+    List<Employee> getEmployeeBySalaryBetween(int salary1,int salary2);
+
+    //Before
+    @Query("SELECT e FROM Employee e WHERE e.hireDate > ?1")
+    List<Employee> getEmployeeByHireDateBefore(LocalDate date);
+
+    //Null
+    @Query("SELECT e FROM Employee e WHERE e.email IS NULL")
+    List<Employee> getEmployeeByEmailIsNull();
+
+    //Not Null
+    @Query("SELECT e FROM Employee e WHERE e.email IS NOT NULL")
+    List<Employee> getEmployeeByEmailIsNotNull();
+
+    //Sort Salary in ascending order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary")
+    List<Employee> getEmployeeBySalaryOrderByAsc();
 
 
 
