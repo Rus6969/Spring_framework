ddb69fcf3dee7bd2bb981d4c3fa85675c0af7885
Russell_Samatov <russam4515@gmail.com>
2021-02-21 21:05:17 -0500

    data addded

---
+++ Spring-09-ORM-Mapping-JPQL-Namedqueries/src/main/java/com/cybertek/entity/Employee.java
@@ -0,0 +1,46 @@
+package com.cybertek.entity;
+
+import com.cybertek.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Employee extends BaseEntity {
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Column(name = "salary")
+    private Integer salary;
+
+
+    @ManyToOne
+    // we need joincolumn here to specify to specify foreign name of deaprtment by defauilt it would create department_id but
+    // since we are using data.sql to pump data there is name department( thats why we are using this annotation)
+    @JoinColumn(name = "department")
+    private Department departments;
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+
+}
