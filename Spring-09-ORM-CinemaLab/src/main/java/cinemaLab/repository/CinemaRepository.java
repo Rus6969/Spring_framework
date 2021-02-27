d4ff75531291397dd7ea48a9475759549b6fc24e
RussellRuslan <SamatovR@aetna.com>
2021-02-27 13:55:35 -0500

    jpql Cinema LAb

--- Spring-09-ORM-CinemaLab/src/main/java/cinemaLab/repository/CinemaRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/cinemaLab/repository/CinemaRepository.java
@@ -2,9 +2,41 @@ package cinemaLab.repository;
 
 import cinemaLab.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema,Long> {
+    // ------------------- DERIVED QUERIES ------------------- //
+
+    //Write a derived query to get cinema with a specific name
+    Optional<Cinema> findByName(String name);
+
+    //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
+    List<Cinema> findFirst3BySponsoredNameContainingOrderBySponsoredNameAsc(String sponsoredname);
+
+    //Write a derived query to list all cinemas in a specific country
+    List<Cinema> findAllByLocationCountry(String country);
+
+    //Write a derived query to list all cinemas with a specific name or sponsored name
+    List<Cinema> findAllByNameOrSponsoredName(String name,String sponsoredname);
+
+    // ------------------- JPQL QUERIES ------------------- //
+
+    //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name FROM Cinema c WHERE c.id=?1")
+    String fetchByIdJPQL(Integer id);
+    //Write a native query to read all cinemas by location country
+    @Query(value = "SELECT * FROM cinema c JOIN location l on l.id=c.location_id WHERE l.country=?1",nativeQuery = true)
+    List<Cinema> retrieveAllByLocationCountry(String locationCountry);
+
+
+
+
+
+
 
 }
