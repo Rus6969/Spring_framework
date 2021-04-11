9c93e1291d2a233d3e37bbcbd684d63024240200
RussellRuslan <SamatovR@aetna.com>
2021-04-10 22:40:55 -0400

    fragments are added

--- Spring-10-Security-CustomConfiguration/src/main/java/com/security2/controller/HomeController.java
+++ Spring-10-Security-CustomConfiguration/src/main/java/com/security2/controller/HomeController.java
@@ -3,13 +3,12 @@ package com.security2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 @RequestMapping("/")
 public class HomeController {
-   @GetMapping("index")
-    public String index(){
 
+    @GetMapping("index")
+    public String index(){
         return "/index";
     }
 }
