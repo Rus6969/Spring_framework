21afd6498440539e853772ca524f0b3d7da9373b
RussellRuslan <SamatovR@aetna.com>
2021-04-19 23:26:40 -0400

    runner class is added

--- Spring-10-Security-Form-Auth/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-10-Security-Form-Auth/src/main/java/com/cybertek/controller/HomeController.java
@@ -5,11 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
     @GetMapping("index")
     public String index(){
         return "/index";
+    }
+
+
+
+
+// if we enter local host or local host login bring login page
+    @GetMapping(value = {"/login","/"})
+    public String login(){
+
+        return "login";
     }
 }
