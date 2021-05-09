16b1f96586f51650226aba847e034e54b0edec76
RussellRuslan <SamatovR@aetna.com>
2021-05-09 15:53:13 -0400

    Schemas dpendencies for open apis is added

--- Spring-11-Rest-Open-Api3.0/src/main/java/com/cybertek/Controller/CinemaController.java
+++ Spring-11-Rest-Open-Api3.0/src/main/java/com/cybertek/Controller/CinemaController.java
@@ -2,11 +2,12 @@ package com.cybertek.Controller;
 
 import com.cybertek.entity.Cinema;
 import com.cybertek.repository.CinemaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 /*/
 to do ducomentation at Rest api3.o
@@ -19,12 +20,15 @@ to do ducomentation at Rest api3.o
  */
 @RestController
 @RequestMapping("/cinema")
+@Tag(name = "Cinema",description = "Cinema Api")
 public class CinemaController {
     @Autowired
     private CinemaRepository cinemaRepository;
 
 
     @GetMapping
+    // this annotation is used to describe api what it does
+    @Operation(summary = "get a all cinimas in all locations in all cities ")
     public List<Cinema> readAll(){
         return cinemaRepository.findAll();
 
--- Spring-11-Rest-Open-Api3.0/src/main/java/com/cybertek/Controller/UserController.java
+++ Spring-11-Rest-Open-Api3.0/src/main/java/com/cybertek/Controller/UserController.java
@@ -2,6 +2,13 @@ package com.cybertek.Controller;
 
 import com.cybertek.entity.User;
 import com.cybertek.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jdk.jfr.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +18,20 @@ import java.util.List;
 
 @RequestMapping("/user")
 @RestController
+@Tag(name = "User",description = "User Api")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping
+    @Operation(summary = "find all users ")
+    // to add 200,400 ets responses
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Successfully retrieved",content = {@Content(mediaType = "application/json",schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400",description = "Something went wrong",content = @Content),
+            @ApiResponse(responseCode = "404",description = "User not found",content = @Content)
+    })
     public List<User>findAllUSers(){
         return userRepository.findAll();
     }
