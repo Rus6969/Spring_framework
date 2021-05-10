df43a8dea25ed52d1ffe588ba5e0cc064d7d5747
RussellRuslan <SamatovR@aetna.com>
2021-05-10 11:27:58 -0400

    Bean is added to create info!

--- Spring-11-Rest-Open-Api3.0/src/main/java/com/cybertek/OpenapiApplication.java
+++ Spring-11-Rest-Open-Api3.0/src/main/java/com/cybertek/OpenapiApplication.java
@@ -1,13 +1,29 @@
 package com.cybertek;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OpenapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenapiApplication.class, args);
+	}
+
+	/*
+	Open api dependecy to add info properties  OpenAPI bean need to be adedded
+	 */
+
+	@Bean
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI()
+				.info(new Info().title("Cinema Application")
+				.version("v1")
+				.description("Selling online cinema tickets "));
+
 	}
 
 }
