0540f4812b56bddb82e8192a24138d915102b1f8
RussellRuslan <SamatovR@aetna.com>
2021-05-16 13:36:07 -0400

    security filter is added

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/config/WebSecurityConfig.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/config/WebSecurityConfig.java
@@ -1,14 +1,19 @@
 package com.cybertek.config;
+import com.cybertek.service.SecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private SecurityFilter securityFilter;
 
     // since we do not have form http we need add this bean , since we do api authentication instrad form login logout ,
     // remeber me bc we are working with api
@@ -29,6 +34,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .anyRequest()
                     .authenticated();
+
+
+          //   after we send a token token to api decoded ( application knows who is user , expired date and status of a user )
+         //   no in filter application checking this
+         // details ,  after filter it wil decide send response or not.
+            http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
     }
+
 
 }
--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/utill/JWTUtil.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/utill/JWTUtil.java
@@ -1,15 +1,23 @@
 package com.cybertek.utill;
 
 import com.cybertek.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
+/*
+1. create token using secret + payload creation what you want to pass
+2. after we create a token and hit an api providing token in a header ( we need decode token for validation against db ( userDetails class is used)
+3. once we deon with token creation token decoding and token validation next step is add FILTERING class located under service :
+ */
 @Component
 public class JWTUtil {
     //this annotation is used bring inject property in a class
@@ -18,22 +26,58 @@ public class JWTUtil {
 
 
     // here we create first part of token PAyload (claims)
-    public String generateToken(User user,String username){
-        Map<String,Object>claims = new HashMap<>();
-        claims.put("username",user.getUsername());
-        claims.put("email",user.getEmail());
-        return createToken(claims,username);
+    public String generateToken(User user, String username) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", user.getUsername());
+        claims.put("email", user.getEmail());
+        return createToken(claims, username);
     }
 
-  // we pass username separatley bc there parameter JWT parameter Subject
-    private String createToken(Map<String,Object> claims,String username){
-            return Jwts
-                    .builder()
-                    .setClaims(claims)
-                    .setSubject(username)
-                    .setIssuedAt(new Date(System.currentTimeMillis()))
-                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *10)) //10 hours token validity
-                    .signWith(SignatureAlgorithm.HS256,secret).compact();
+    // we pass username separatley bc there parameter JWT parameter Subject
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) //10 hours token validity
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
 
-        }
+    /*
+    after we create token and send it user hit api , before token verification we need   to decode token
+    in this method we getting body of a claim ( payload)
+     */
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    //functional interfaces only abstract method existed with one abstract method , NOOOOOTCLEAR
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+
+    // we are adding user details bc spring needs to evaluate  a token after extraction we have user, email etc now it need to do validation against DB
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+
 }
