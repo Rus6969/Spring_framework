ffae42676fa248d79879969a8ef430214ad16e9a
RussellRuslan <SamatovR@aetna.com>
2021-07-30 13:29:21 -0400

    student entity is added

--- Spring-13-UnitTesting-MockMvc/src/main/java/com/cybertek/entity/Student.java
+++ Spring-13-UnitTesting-MockMvc/src/main/java/com/cybertek/entity/Student.java
@@ -8,13 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
